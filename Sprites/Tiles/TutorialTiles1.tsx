3d1b14a3f6fd97abe7d6a99222be846f6c451a55
Jane Henson <122830317+shegel99@users.noreply.github.com>
2023-07-05 11:32:28 -0700

    added clutter and background layer/tilesets

---
+++ Sprites/Tiles/TutorialTiles1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="TutorialTiles1" tilewidth="32" tileheight="32" tilecount="70" columns="10">
+ <image source="Tutorial Tiles/TutorialTiles1.png" width="350" height="230"/>
+</tileset>

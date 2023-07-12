6767ebc46737ee456a8698dcb1881039f906055d
Michael <rarrafarrar@gmail.com>
2023-07-12 15:37:35 -0700

    Deleted redundant folder

--- Sprites/Tiles/test tiles/TEST4.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="TEST4" tilewidth="32" tileheight="32" tilecount="70" columns="10">
- <image source="test4.png" width="350" height="230"/>
-</tileset>
--- Sprites/Tiles/walls.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="walls" tilewidth="32" tileheight="96" tilecount="24" columns="12">
- <image source="Tutorial Tiles/walls.png" width="414" height="230"/>
-</tileset>

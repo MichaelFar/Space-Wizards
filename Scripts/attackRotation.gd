e3dfdfebfc741ff63d684526d761acf1f400d9d2
Michael <rarrafarrar@gmail.com>
2023-07-12 16:03:34 -0700

    Cleaned directory, please do not break

--- Scripts/attackRotation.gd
+++ Scripts/attackRotation.gd
@@ -1,10 +1,23 @@
 extends CharacterBody2D
 
 
+var attackPlayer = null
+var playerSpriteTree = null
+var animationState = null
+var playerNode = null
+
 func _ready():
-	pass
+	playerNode = get_owner()
+	attackPlayer = $"../AttackSpritePlayer"#Get sibling
+	playerSpriteTree = $"../PlayerSpriteAnimTree"
+	animationState = playerSpriteTree.get("parameters/playback")
 
-
-func _physics_process(_delta):
+func _physics_process(delta):
 	
-	self.look_at(get_global_mouse_position())
+	if(attackPlayer.current_animation_position == 0 && attackPlayer.current_animation_length != 0):
+		self.look_at(get_global_mouse_position())
+		playerSpriteTree.set("parameters/IdleBlend/blend_position", playerNode.get_local_mouse_position())
+		animationState.travel("IdleBlend")
+		
+	
+	
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,6 +6,8 @@ var input_vector = Vector2.ZERO
 const acceleration = 700 #Multiplied by delta
 const friction = 250 #Multiplied by delta
 const max_speed = 150 # NOT multiplied by delta
+const attack_movement = 50 #Multiplied by delta
+
 
 var playerSpritePlayer = null
 var playerSpriteTree = null
@@ -13,15 +15,17 @@ var animationState = null
 var attackSpritePlayer = null
 var playerPosition = Vector2.ZERO
 var frameRecovery = 0
+var mouse_coordinates = Vector2.ZERO
+var previousBlend = Vector2.ZERO
+
 
 var state = MOVE
 
 enum {
 	MOVE,
-	DODGE,
+	DRIFT,
 	ATTACK
 }
-
 
 func _ready():#Called when node loads into the scene, children ready functions run first
 	
@@ -34,21 +38,19 @@ func _ready():#Called when node loads into the scene, children ready functions r
 func post_initialize(animation_tree):#Bug in godot where i needed to wrap this in a function to get it to run
 	animationState = animation_tree.get("parameters/playback")
 
-func _physics_process(_delta):#Runs per frame (delta is the difference in time between the current frame and the last frame)
+func _physics_process(_delta):#Runs per frame, contains starting player state machine
 	
 	match state:
 		
 		MOVE:
 			move_state(_delta)
 			
-		DODGE:
+		DRIFT:
 			pass
 			
 		ATTACK:
 			attack_state(_delta)
-			state = MOVE
-	
-
+		
 func move_state(_delta):
 	
 	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
@@ -58,39 +60,48 @@ func move_state(_delta):
 	
 	playerPosition = self.position
 	
-	playerSpriteTree.set("parameters/IdleBlend/blend_position", input_vector)
-	animationState.travel("IdleBlend")
-	#Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
-	
+	#playerSpriteTree.set("parameters/IdleBlend/blend_position", input_vector)
+	#animationState.travel("IdleBlend")
 	
 	if (Input.is_action_just_pressed("click")):
 
 		state = ATTACK
-
+		mouse_coordinates = get_local_mouse_position()
+		velocity = Vector2.ZERO
 	if input_vector != Vector2.ZERO:
 		
 		playerSpriteTree.set("parameters/IdleBlend/blend_position", input_vector)
 		playerSpriteTree.set("parameters/MoveBlend/blend_position", input_vector)
 		animationState.travel("MoveBlend")
+		previousBlend = input_vector
 		velocity = velocity.move_toward(input_vector * max_speed, acceleration * _delta)
 		
-		print("Moving towards " + " " + str(velocity))
+		print("Moving towards " + str(velocity))
 	
-	elif(velocity != Vector2.ZERO):
+	elif(velocity != Vector2.ZERO && state != ATTACK):
 		
+		playerSpriteTree.set("parameters/IdleBlend/blend_position", previousBlend)
 		animationState.travel("IdleBlend")
 		velocity = velocity.move_toward(Vector2.ZERO, friction * _delta)
 	
-	else:
-		
-		animationState.travel("IdleBlend")
-		
+	print("Mouse coordinates in move state " + str(mouse_coordinates))
 	print("Current velocity vector", velocity)	
 	#move_and_collide(velocity * _delta)
 	move_and_slide()
 
-func attack_state(_delta):
-	attackSpritePlayer.play("melee_attack")
+func attack_state(_delta):#State machine for attack combos will go here
 	
+	if(attackSpritePlayer.current_animation_position == 0):
+		attackSpritePlayer.play("melee_attack")
+		
+	elif(attackSpritePlayer.current_animation_position < attackSpritePlayer.current_animation_length):
+		print("Now applying attack momentum, velocity = " + str(velocity))
+		velocity = velocity.move_toward(mouse_coordinates, attack_movement * _delta)
+	elif(attackSpritePlayer.current_animation_position == attackSpritePlayer.current_animation_length):
+		attackSpritePlayer.stop()
+		state = MOVE
+		#velocity = Vector2.ZERO
+	move_and_slide()
 	
-	
+	print("Velocity while attacking " + str(velocity))
+	print("Mouse position in attack state " + str(mouse_coordinates))	

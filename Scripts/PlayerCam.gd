509a140d699900b3bc8ef275826ec44de67fb0fe
MichaelFar <rarrafarrar@gmail.com>
2023-09-07 10:39:40 -0700

    Added relative camera velocity based on mouse position, changed viewport size

--- Scripts/PlayerCam.gd
+++ Scripts/PlayerCam.gd
@@ -5,24 +5,41 @@ extends Camera2D
 
 @export var shakeStrength = 10.0
 
-@export var cameraThreshold = 125
+@export var cameraThreshold = 200
 
-@export var cameraVelocity = 300
+@export var maxCameraVelocity = 500
+
+var mouseRatio = 0.0
+
+var parent = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	parent = get_parent()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var cameraVelocity = maxCameraVelocity
+	var maxMousePosition = Vector2.ZERO.distance_to(Vector2(get_viewport_rect().end.x , get_viewport_rect().end.y))
 	
-	if(get_local_mouse_position().distance_to(position) > cameraThreshold):
-		offset = offset.move_toward(Vector2((get_local_mouse_position().normalized() * cameraThreshold).x, (get_local_mouse_position().normalized() * cameraThreshold).y) / 2, delta * cameraVelocity)
+	print(str(maxMousePosition))
+	mouseRatio = clamp(Vector2.ZERO.distance_to(get_local_mouse_position()) / maxMousePosition, 0.0, 1.0)
+	print("Mouse ratio is " + str(mouseRatio) + " and " + str(Vector2.ZERO.distance_to(get_local_mouse_position()) / maxMousePosition) + " and " + str(get_local_mouse_position()))
+	
+	cameraVelocity = maxCameraVelocity * mouseRatio
+	
+	offset = offset.move_toward(
+	Vector2((get_local_mouse_position().normalized() * cameraThreshold).x * mouseRatio, 
+	(get_local_mouse_position().normalized() * cameraThreshold).y) * mouseRatio, 
+	delta * cameraVelocity)
+	
+	if(Input.is_action_just_released("CameraToggle")):
+		enabled = !enabled
 	
 	if(shouldShake):
-		shake(delta)
+		shake()
 	#offset = clamp(offset, Vector2(-cameraThreshold, -cameraThreshold) + global_position, Vector2(cameraThreshold, cameraThreshold) + global_position)
 
-func shake(delta):
+func shake():
 
 	var rObj = RandomNumberGenerator.new()
 	
